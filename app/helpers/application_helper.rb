21eb963e4383bc280454f110c0e39bbcab237257
Mattia Roccoberton <mattia.roccoberton@develon.com>
2016-10-02 12:04:51 +0200

    Added slim_comp_render helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def slim_comp_render( comp )
+    Slim::Template
+      .new( Rails.application.config.components_path.join( comp ) )
+      .render
+      .gsub( '"', '\"' )
+  end
 end
